bd8b5b43583783d275d3d5cfe4cb0feee408e03f
Prateek Shourya <prateekshourya29@gmail.com>
2021-06-19 00:03:27 +0530

    :iphone: Work on responsive design.

--- src/App.tsx
+++ src/App.tsx
@@ -16,15 +16,14 @@ const App: React.FC = () => {
   const [res, updateRes] = React.useState<FetchResponse[]>([]);
   const [loading, updateLoading] = React.useState<boolean>(false);
 
-  const searchTvShows = async (query: string): Promise<any> => {
-    if (query === "") return;
-
-    console.log("Searching for...", query);
+  const searchTvShows = async (query: string) => {
     updateLoading(true);
+
     const res = await axios.get(
       `http://api.tvmaze.com/search/shows?q=${query}`
     );
     updateRes(res.data);
+
     updateLoading(false);
   };
 
@@ -37,21 +36,39 @@ const App: React.FC = () => {
     <Row>
       <Col>
         <Text variant="h4" textAlign="center">
-          TV Show Search App
+          Search TV Show
         </Text>
       </Col>
-      <Col span={10} offset={7}>
+      <Col
+        spanXs={22}
+        spanSm={20}
+        spanMd={16}
+        spanLg={12}
+        spanXl={10}
+        offset={{ xs: 1, sm: 2, md: 4, lg: 6, xl: 7 }}
+      >
         <form onSubmit={(e) => handleSubmit(e)}>
           <Row>
-            <Col offset={2} span={20}>
+            <Col
+              spanXs={20}
+              spanSm={18}
+              spanXl={16}
+              offset={{ xs: 2, sm: 3, xl: 4 }}
+            >
               <Input
                 value={InputValue}
                 onChange={(e) => updateInputValue(e.target.value)}
-                label="Search for Tv Shows"
+                label="Enter Show Name"
               />
             </Col>
-            <Col offset={10} span={4}>
+            <Col
+              spanXs={8}
+              spanSm={8}
+              spanXl={6}
+              offset={{ xs: 8, sm: 8, xl: 9 }}
+            >
               <Button
+                displayBlock
                 type="submit"
                 primary
                 loading={loading}
@@ -63,10 +80,17 @@ const App: React.FC = () => {
           </Row>
         </form>
       </Col>
-      <Col span={16} offset={4}>
-        <Row>
+      <Col spanXs={22} spanSm={22} spanXl={20} offset={{ xs: 1, sm: 1, xl: 2 }}>
+        <Row gutter={[15, 15]}>
           {res.map((v) => (
-            <Col span={4.8}>
+            <Col
+              spanXs={16}
+              spanSm={11}
+              spanMd={8}
+              spanLg={6}
+              spanXl={4.8}
+              offset={{ xs: 4, sm: 1, md: 0 }}
+            >
               <Card>
                 <CardMedia
                   image={
