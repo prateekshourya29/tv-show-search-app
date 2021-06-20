9496267626612408e31524926dacfbe438d49a03
Prateek Shourya <prateekshourya29@gmail.com>
2021-06-20 17:20:44 +0530

    :sparkles: Add google page for cast members.

--- src/App/Components/CastCard.tsx
+++ src/App/Components/CastCard.tsx
@@ -3,18 +3,24 @@ import { Card, CardHeader, CardMedia, Text } from "sha-el-design/lib";
 import { CastDetails } from "../Typings/Shows";
 
 const CastCard = (props: { data: CastDetails }) => (
-  <Card>
-    <CardMedia image={props.data.person?.image?.medium} height="200px" />
-    <CardHeader>
-      <div style={{ width: "90%" }}>
-        <Text>{props.data.person.name}</Text>
-      </div>
-    </CardHeader>
-    <Text>
-      as
-      <br /> {props.data.character.name}
-    </Text>
-  </Card>
+  <a
+    href={`https://www.google.com/search?q=${props.data.person.name}`}
+    target="_blank"
+    rel="noreferrer"
+  >
+    <Card>
+      <CardMedia image={props.data.person?.image?.medium} height="200px" />
+      <CardHeader>
+        <div style={{ width: "90%" }}>
+          <Text>{props.data.person.name}</Text>
+        </div>
+      </CardHeader>
+      <Text>
+        as
+        <br /> {props.data.character.name}
+      </Text>
+    </Card>
+  </a>
 );
 
 export default CastCard;
